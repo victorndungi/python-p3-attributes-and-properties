b5585ed33f9ce745654a5ebb5bc03cca11aaa17a
victorndungi <victor.ndungi@student.moringaschool.com>
2024-09-18 12:18:36 +0300

    Labs

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Guido", job="Sales"):
+
+        self.name = name
+        self.job = job
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <=25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+
+
